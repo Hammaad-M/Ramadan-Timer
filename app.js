381d2d41c5ef4ae265e228da47f8ab87babd10f8
Hammaad-M <hammaad.memon@gmail.com>
2021-03-26 14:26:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,119 @@
+let prayerTimes = [];
+let rawPrayerTimes = [];
+let now = new Date();
+let nextPrayer;
+const prayers = ["Fajr/Sunrise", "Maghrib/Sunset"];
+const countdown = document.getElementById("countdown");
+const nextPrayerDisplays = document.querySelectorAll(".next-prayer");
+const prayerTimeDisplay = document.getElementById("prayer-time");
+
+console.log(nextPrayerDisplays)
+
+async function getTimes() {
+  if (!('fetch' in window)) {
+    alert("Fetch API disabled or not found...unable to get time remaining.");
+    return;
+  }
+  let times = [];
+  return new Promise(async (resolve) => {
+    await jQuery(async ($) => {
+      $.getJSON('http://muslimsalat.com/seattle.json?jsoncallback=?', (response) => {
+        times.push(response.items[0].fajr, response.items[0].maghrib);
+        resolve(times);
+      });
+    });
+  });
+}
+
+function update() {
+  let now = new Date();
+  remaining = msToTime(prayerTimes[getNextPrayer()] - now);
+  countdown.textContent = `${format(remaining.hours)}:${format(remaining.minutes)}:${format(remaining.seconds)}`;
+}
+
+async function init() {
+  const times = await getTimes();
+  times.forEach((time) => {
+    rawPrayerTimes.push(time);
+    prayerTimes.push(getPrayerDate(time));
+  });
+  msToFajr = now - prayerTimes[0];
+  update();
+  setInterval(() => update(), 1000);
+};
+
+
+
+function getPrayerDate(time) {
+  let string = time;
+  let objectTimeCode = time.substr(time.length-2);
+  if (objectTimeCode == "pm") {
+    let modifiedHours = parseInt(time.substr(0, 2))+12;
+    let newTime = modifiedHours.toString() + ":" + time.substr(2);
+    string = newTime.substring(0, newTime.length-3);
+  } else {
+    string = string.substring(0, string.length-3);
+  }
+  console.log(string);
+  const hours = string.substr(0, (string.length == 5) ? 2 : 1);
+  const minutes = string.substr(string.length-2, 2);
+  console.log(hours, minutes);
+  let date = new Date(now.toDateString());
+  date.setHours(hours, minutes, 0);
+  console.log(msToTime(date - now));
+  if (date - now < 0) {
+    date.setDate(now.getDate() + 1); 
+  }
+  console.log(msToTime(date - now));
+  console.log(date.toDateString() + " | " + date.getHours() + ":" + date.getMinutes());
+  return date;
+}
+
+function msToTime(ms) {
+  console.log(ms);
+  ms = Math.abs(ms);
+  let seconds = ms/1000;
+  let minutes = seconds/60;
+  let hours = minutes/60;
+  seconds = Math.trunc(seconds % 60)
+  minutes = Math.trunc(minutes % 60)
+  hours = Math.trunc(hours);
+  if (seconds == 60) {
+    minutes+=1;
+    seconds = 0;
+  }
+  if (minutes == 60) {
+    hours+=1;
+    minutes = 0;
+  }
+  return {hours, minutes, seconds};
+}
+
+function getNextPrayer() {
+  let closest = prayerTimes[0] - now;
+  let closestPrayer;
+  console.log(prayerTimes);
+  prayerTimes.forEach((time, i) => {
+    if (time - now < closest) {
+      closest = time;
+      closestPrayer = prayers[i];
+    }
+  });
+  if (closestPrayer != nextPrayer) {
+    nextPrayer = closestPrayer;
+    nextPrayerDisplays.forEach((display) => {
+      display.textContent = nextPrayer;
+    });
+    prayerTimeDisplay.textContent = rawPrayerTimes[prayers.indexOf(closestPrayer)];
+  }
+  console.log(closestPrayer);
+  return prayers.indexOf(closestPrayer);
+}
+
+function format(string) {
+  if (string < 10) {
+    return "0" + string;
+  } else {
+    return string;
+  }
+}
