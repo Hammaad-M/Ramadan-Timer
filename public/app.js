fd07df3977456da21fdbe4cd60bca4fc2de7fd29
Hammaad <hammaad.memon@gmail.com>
2023-04-03 18:14:33 -0700

    add custom countdown feature

--- public/app.js
+++ public/app.js
@@ -14,6 +14,8 @@ const adhanOptions = document.getElementById("adhan-options");
 const dateDisplay = document.getElementById("date");
 const progressBar = document.querySelector(".progress-bar");
 const progressDisplay = document.querySelector("#progress-display");
+const star = document.querySelector(".star");
+const offsetDisplay = document.querySelector(".offset-display");
 document.getElementById("adhan-off").checked = true;
 let TIDs = [];
 const days = [
@@ -79,6 +81,92 @@ let useIP;
 let fastDuration;
 let todayFajrTime;
 let initializing = false;
+
+let offset = 0;
+
+function addMillisecondsToTimeString(timeString, milliseconds) {
+  // Split the time string into hours, minutes, and AM/PM
+  const [time, ampm] = timeString.split(" ");
+  const [hours, minutes] = time.split(":").map((str) => parseInt(str, 10));
+
+  // Convert the hours to 24-hour format
+  let hours24 = hours;
+  if (ampm === "PM" && hours !== 12) {
+    hours24 += 12;
+  } else if (ampm === "AM" && hours === 12) {
+    hours24 = 0;
+  }
+
+  // Calculate the total number of milliseconds since midnight
+  const totalMilliseconds = (hours24 * 60 + minutes) * 60 * 1000 + milliseconds;
+
+  // Convert the total milliseconds back to hours, minutes, and AM/PM
+  const newHours24 = Math.floor(totalMilliseconds / (60 * 60 * 1000)) % 24;
+  const newMinutes = Math.floor(totalMilliseconds / (60 * 1000)) % 60;
+  const newAmPm = newHours24 < 12 ? "AM" : "PM";
+  const newHours = newHours24 % 12 || 12;
+
+  // Combine the new hours, minutes, and AM/PM into a string and return it
+  const newTime = `${newHours}:${newMinutes
+    .toString()
+    .padStart(2, "0")} ${newAmPm}`;
+  return newTime;
+}
+
+const updateOffset = (change) => {
+  const milliseconds = change * 1000 * 60;
+  offset += milliseconds;
+  const offsetMinutes = offset / 1000 / 60;
+  star.style.display = "block";
+  offsetDisplay.textContent = offsetMinutes;
+  const newTime = addMillisecondsToTimeString(
+    prayerTimeDisplay.textContent,
+    milliseconds
+  );
+  prayerTimeDisplay.textContent = newTime;
+  if (offsetMinutes === 0) {
+    if (nextPrayerIndex === 0) {
+      document.querySelector(".star-0").style.display = "none";
+    } else {
+      document.querySelector(".star-3").style.display = "none";
+    }
+  } else {
+    // if fajr is next
+
+    if (nextPrayerIndex === 0) {
+      document.getElementById("prayer-time-0").textContent = newTime;
+      document.querySelector(".star-0").style.display = "block";
+    } else {
+      document.getElementById("prayer-time-3").textContent = newTime;
+      document.querySelector(".star-3").style.display = "block";
+    }
+  }
+
+  // offsetDisplay.textContent = prayerTimeDisplay.textContent;
+};
+
+const resetOffset = () => {
+  const oldTime = addMillisecondsToTimeString(
+    prayerTimeDisplay.textContent,
+    offset * -1
+  );
+  prayerTimeDisplay.textContent = oldTime;
+  // offsetDisplay.textContent = prayerTimeDisplay.textContent;
+  star.style.display = "none";
+  offsetDisplay.textContent = 0;
+  offset = 0;
+  try {
+    if (nextPrayerIndex === 0) {
+      document.getElementById("prayer-time-0").textContent = oldTime;
+      document.querySelector(".star-0").style.display = "none";
+    } else {
+      document.getElementById("prayer-time-3").textContent = oldTime;
+      document.querySelector(".star-3").style.display = "none";
+    }
+  } catch (err) {
+    console.warn("error handling stars...", err);
+  }
+};
 
 function setMidnight() {
   midnight = new Date(
@@ -227,12 +315,13 @@ function update(times) {
     if (pauseCounter == 19) {
       $("#dua").remove();
     }
-    remaining = msToTime(prayerTimes[nextPrayerIndex] - now);
+    // console.log(prayerTimes[nextPrayerIndex]);;
+    remaining = msToTime(prayerTimes[nextPrayerIndex] - now + offset);
 
     // console.log(fastDuration, prayerTimes[1] - now);
 
     updateProgressBar(
-      fastDuration,
+      fastDuration + offset,
       nextPrayerIndex === 1 ? now - todayFajrTime : now - yesterdayMaghrib
     );
 
@@ -244,6 +333,12 @@ function update(times) {
     if (remaining.hours == 0 && remaining.minutes == 0) {
       if (remaining.seconds == 0) {
         endCountdown();
+        resetOffset();
+        if (nextPrayerIndex !== 0) {
+          yesterdayMaghrib = new Date();
+        } else {
+          todayFajrTime = new Date();
+        }
         setNextPrayer(false, times);
       } else {
         createAlertEffects(remaining.seconds);
@@ -306,6 +401,7 @@ function resetContent() {
 }
 async function init(city) {
   resetContent();
+  resetOffset();
   toggleLoadingScreen();
   if (city != null && city == myCity) {
     city = null;
@@ -602,7 +698,9 @@ function charCount(str, letter) {
   return letterCount;
 }
 function msToTime(ms) {
+  // console.log(ms);
   ms = Math.abs(ms);
+  // console.log(ms);
   let seconds = ms / 1000;
   let minutes = seconds / 60;
   let hours = minutes / 60;
@@ -656,6 +754,7 @@ function setNextPrayer(first, times) {
   let time =
     fajrTommorow && nextPrayerIndex == 0 ? nextFajrTime : times[getIndex()];
   prayerTimeDisplay.textContent = to12hrDisplayTime(time).toUpperCase();
+  // offsetDisplay.textContent = prayerTimeDisplay.textContent;
 }
 function format(string) {
   if (string < 10) {
@@ -765,9 +864,24 @@ function displayAllPrayerTimes(times) {
   document.getElementById("all-prayer-times").innerHTML = "";
   times.forEach((time, i) => {
     $("#all-prayer-times").append(
-      `<div><p>${prayerList[i]}: &#8287&#8287&#8287 <span>${to12hrDisplayTime(
+      `<div><p>${
+        prayerList[i]
+      }: &#8287&#8287&#8287 <span id="prayer-time-${i}">${to12hrDisplayTime(
         time
-      )}</span></p></div>`
+      )}</span></p><svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 1024 1024"
+      height="1em"
+      width="1em"
+      class="hidden table-star star-${i}"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"
+      ></path>
+    </svg></div>`
     );
   });
 }
